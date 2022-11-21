07de81223813de46c9a71eceaf9597e68a1d5e80
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2022-11-21 13:49:30 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { } from 'react-icons'
+
+function App() {
+  return (
+    <div className="App">
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
