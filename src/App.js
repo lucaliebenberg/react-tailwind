afe2453ec475eff38cd424b168de6bbf3ad49bfc
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2022-12-01 14:34:39 +0200

    Hero component add (search component)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 // import { } from 'react-icons'
 
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { heroImage } from "../assets/heroImage.jpg";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col justify-center text-center align-middle pt-14">
+      <h1 className="text-4xl">Have a look around</h1>
+      <div>
+        <input
+          className="border-gray border-2 rounded-md w-[60%] mt-3 p-1"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
